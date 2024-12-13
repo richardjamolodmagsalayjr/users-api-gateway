0e500f5309dece4f6f877a8dd490fa470c26a491
Richard J. Magsalay Jr. <richardjr.magsalay@g.msuiit.edu.ph>
2024-12-13 15:57:46 +0800

    Initial commit.

---
+++ src/main/java/com/rmagsalay/users_api_gateway/UsersApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.rmagsalay.users_api_gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsersApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsersApiGatewayApplication.class, args);
+	}
+
+}
